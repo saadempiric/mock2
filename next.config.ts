46ba02d80932b662ea96a78e62aadfda2cdf96b5
saadempiric <sasalman1@empirictech.com>
2025-03-13 13:12:33 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,54 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: `
+              default-src 'self';
+              script-src 'self' 'unsafe-inline' 'unsafe-eval' 
+                https://*.pusher.com 
+                https://teams.microsoft.com 
+                https://www.google.com 
+                https://www.gstatic.com 
+                https://consent.cookiebot.com;
+              connect-src 'self' 
+                https://*.pusher.com 
+                wss://*.pusher.com 
+                https://teams.microsoft.com 
+                https://www.google.com 
+                https://www.gstatic.com 
+                https://ipwho.is;
+              frame-src 'self' 
+                https://teams.microsoft.com 
+                https://www.google.com;
+              style-src 'self' 'unsafe-inline' 
+                https://teams.microsoft.com 
+                https://www.google.com;
+              img-src 'self' data: 
+                https://www.gstatic.com;
+            `.replace(/\s{2,}/g, " "), // Minify CSP string
+          },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://teams.microsoft.com",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
